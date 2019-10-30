5ca13cf18e4451bfd31771d0bc9ddc1a2f8ea6ff
Flávio Carvalho <flaviocfr@gmail.com>
2019-10-30 15:37:54 -0300

    Commit bruto

--- src/main/java/com/reobotdev/HelpDesk2/HelpDesk2Application.java
+++ src/main/java/com/reobotdev/HelpDesk2/HelpDesk2Application.java
@@ -16,14 +16,13 @@ public class HelpDesk2Application implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(HelpDesk2Application.class, args);
 	}
 	
 	@Override
 	public void run(String... args) throws Exception {
-
+		
 		User user1 = new User( null,"a@a.com", "12345678", ProfileEnum.ROLE_ADMIN);
 		User user2 = new User( null,"a2@a.com", "12345678", ProfileEnum.ROLE_ADMIN);
 
