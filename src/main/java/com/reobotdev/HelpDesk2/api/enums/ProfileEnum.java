648664a85bc329cd76774378e3834c93683915ee
Flávio Carvalho <flaviocfr@gmail.com>
2019-10-31 01:07:25 -0300

    Refactoring ProfileEnum

--- src/main/java/com/reobotdev/HelpDesk2/api/enums/ProfileEnum.java
+++ src/main/java/com/reobotdev/HelpDesk2/api/enums/ProfileEnum.java
@@ -1,9 +1,41 @@
 package com.reobotdev.HelpDesk2.api.enums;
 
 public enum ProfileEnum {
-	
-	ROLE_ADMIN,
-	ROLE_CUSTOMER,
-	ROLE_TECNICIAN
 
-}
+		ADMIN(1, "ROLE_ADMIN"),
+		CUSTOMER(2, "ROLE_CUSTOMER"),
+	    TECNICIAN(3, "ROLE_TECNICIAN");
+		
+
+		private int cod;
+		private String descricao;
+
+		private ProfileEnum(int cod, String descricao) {
+			this.cod = cod;
+			this.descricao = descricao;
+		}
+
+		public int getCod() {
+			return cod;
+		}
+
+		public String getDescricao () {
+			return descricao;
+		}
+
+		public static ProfileEnum toEnum(Integer cod) {
+
+			if (cod == null) {
+				return null;
+			}
+
+			for (ProfileEnum  x : ProfileEnum.values()) {
+				if (cod.equals(x.getCod())) {
+					return x;
+				}
+			}
+
+			throw new IllegalArgumentException("Id inválido: " + cod);
+		}
+
+	}
